75254cbf7f3dcb9aa58aabf0d5c618a0cb2f5b0a
GeneralZYQ <zhangyuanqing.generalzyq@gmail.com>
2019-06-13 20:55:50 +0200

    The initial of the project. I will commit the README file asap

---
+++ tecalpy.py
@@ -0,0 +1,26 @@
+import os
+import csv
+
+# name = 'name %s , big %s' % ('hah', 'xi')
+# print (name)
+# os.system('python test.py')
+
+# for x in range(1,3):
+# 	if x == 1:
+# 		os.popen('python mto.py gray1f.fits -verbosity 2 -out out1.png -par_out pout1.csv')
+
+# 	if x == 2:
+# 		os.popen('python mto.py gray2f.fits -verbosity 2 -out out2.png -par_out pout2.csv')
+
+# with open('caste.csv', 'a') as csvfile:
+#         param_writer = csv.writer(csvfile)
+
+#         param_writer.writerow(['heading1', 'heading2'])
+
+# with open('caste.csv', 'a') as csvfile:
+#         param_writer = csv.writer(csvfile)
+
+#         param_writer.writerow(['value1', 'value2'])
+
+
+
---
+++ test.py
@@ -0,0 +1,9 @@
+#testpy
+print ("Hehe");
+
+import csv 
+import pandas as pd
+
+df = pd.read_csv('indices_587744874251944271.csv')
+
+print (df.shape)
